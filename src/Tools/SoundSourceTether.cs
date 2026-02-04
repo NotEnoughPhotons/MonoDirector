27eefae75660e334d7edc93023e9fdde0fd8d8e6
adamdev <adamdevofficial@protonmail.com>
2026-02-04 12:36:45 -0500

    Sound source tether now attaches to actors!

--- src/Tools/SoundSourceTether.cs
+++ src/Tools/SoundSourceTether.cs
@@ -15,7 +15,9 @@ namespace NEP.MonoDirector.Tools
 
         private Grip m_tetherGrip;
         private Rigidbody m_tetherBody;
+        private Rigidbody m_originalConnectedBody;
         private ConfigurableJoint m_tetherJoint;
+        private GameObject m_tetherMesh;
 
         private Action<Hand> m_OnTetherGrabbed;
         private Action<Hand> m_OnTetherReleased;
@@ -33,22 +35,36 @@ namespace NEP.MonoDirector.Tools
             m_tetherGrip = GetComponent<Grip>();
             m_tetherBody = GetComponent<Rigidbody>();
             m_tetherJoint = GetComponent<ConfigurableJoint>();
+            m_tetherMesh = transform.Find("Sphere").gameObject;
+
+            m_originalConnectedBody = m_tetherJoint.connectedBody;
         }
 
         private void OnEnable()
         {
             m_tetherGrip.attachedHandDelegate += m_OnTetherGrabbed;
             m_tetherGrip.detachedHandDelegate += m_OnTetherReleased;
+
+            m_tetherJoint.connectedBody = m_originalConnectedBody;
         }
 
         private void OnDisable()
         {
             m_tetherGrip.attachedHandDelegate -= m_OnTetherGrabbed;
             m_tetherGrip.detachedHandDelegate -= m_OnTetherReleased;
+
+            m_attachedActor.Microphone.AssignSound(null);
+
+            m_source.Unmute();
         }
 
         protected void OnHandAttached(Hand hand)
         {
+            if (m_attachedActor != null)
+            {
+                return;
+            }
+
             m_tetherJoint.xMotion = ConfigurableJointMotion.Limited;
             m_tetherJoint.yMotion = ConfigurableJointMotion.Limited;
             m_tetherJoint.zMotion = ConfigurableJointMotion.Limited;
@@ -56,12 +72,22 @@ namespace NEP.MonoDirector.Tools
 
         protected void OnHandDetached(Hand hand)
         {
+            if (m_attachedActor != null)
+            {
+                return;
+            }
+
             m_tetherJoint.xMotion = ConfigurableJointMotion.Limited;
             m_tetherJoint.yMotion = ConfigurableJointMotion.Limited;
             m_tetherJoint.zMotion = ConfigurableJointMotion.Limited;
         }
         private void OnTetherGrabbed(Hand hand)
         {
+            if (m_attachedActor != null)
+            {
+                UnTether();
+            }
+
             m_tetherJoint.xMotion = ConfigurableJointMotion.Free;
             m_tetherJoint.yMotion = ConfigurableJointMotion.Free;
             m_tetherJoint.zMotion = ConfigurableJointMotion.Free;
@@ -72,6 +98,14 @@ namespace NEP.MonoDirector.Tools
             if (m_hoveredActor != null)
             {
                 Tether();
+            }
+
+            if (m_attachedActor != null)
+            {
+                m_tetherJoint.xMotion = ConfigurableJointMotion.Free;
+                m_tetherJoint.yMotion = ConfigurableJointMotion.Free;
+                m_tetherJoint.zMotion = ConfigurableJointMotion.Free;
+                return;
             }
 
             m_tetherJoint.xMotion = ConfigurableJointMotion.Limited;
@@ -112,7 +146,6 @@ namespace NEP.MonoDirector.Tools
         private void OnTetherTriggerEnter(Actor actor)
         {
             m_hoveredActor = actor;
-            Main.Logger.Msg(actor.ActorName);
         }
 
         private void OnTetherTriggerExit(Actor actor)
@@ -124,11 +157,34 @@ namespace NEP.MonoDirector.Tools
         {
             m_hoveredActor.Microphone.AssignSound(m_source.Clip);
             m_hoveredActor.Microphone.SetCorrectionMode(ActorSpeech.AudioCorrectionMode.NonCorrected);
-            Main.feedbackSFX.LinkAudio();
             m_attachedActor = m_hoveredActor;
+
+            m_tetherJoint.connectedBody = m_hoveredActor.ActorBody.Head.GetComponent<Rigidbody>();
+
+            Main.feedbackSFX.LinkAudio();
             m_hoveredActor = null;
 
             m_source.Mute();
+        }
+
+        private void UnTether()
+        {
+            m_tetherJoint.connectedBody = m_originalConnectedBody;
+            m_attachedActor.Microphone.AssignSound(null);
+            m_attachedActor = null;
+        }
+
+        public void Hide()
+        {
+            // This code exists solely to keep the config joint alive.
+            m_tetherMesh.SetActive(false);
+            m_tetherGrip.enabled = false;
+        }
+
+        public void Show()
+        {
+            m_tetherMesh.SetActive(true);
+            m_tetherGrip.enabled = true;
         }
     }
 }
