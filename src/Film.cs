b8a0e19f2f44984c79e95a2167466a29fe0e8a76
adamdev <adamdevofficial@protonmail.com>
2026-03-03 05:27:19 -0500

    Added film and stage classes

---
+++ src/Film.cs
@@ -0,0 +1,19 @@
+﻿namespace NEP.MonoDirector.Core
+{
+    public sealed class Film
+    {
+        public Film()
+        {
+            m_stages = new List<Stage>();
+        }
+
+        public Film(List<Stage> stages)
+        {
+            m_stages = stages;
+        }
+
+        public IReadOnlyList<Stage> Stages => m_stages.AsReadOnly();
+
+        private List<Stage> m_stages;
+    }
+}
---
+++ src/Stage.cs
@@ -0,0 +1,41 @@
+﻿using Il2CppSLZ.Marrow.Warehouse;
+using NEP.MonoDirector.Actors;
+
+namespace NEP.MonoDirector.Core
+{
+    public sealed class Stage
+    {
+        public Stage()
+        {
+            m_actors = new List<Actor>();
+        }
+
+        public IReadOnlyList<Actor> Actors => m_actors.AsReadOnly();
+        public Barcode LevelBarcode => m_levelBarcode;
+        public string Name => m_name;
+
+        private List<Actor> m_actors;
+        private Barcode m_levelBarcode;
+        private string m_name;
+
+        public void AddActor(Actor actor)
+        {
+            m_actors.Add(actor);
+        }
+
+        public void AddActors(List<Actor> actors)
+        {
+            m_actors.AddRange(actors);
+        }
+
+        public void RemoveActor(Actor actor)
+        {
+            m_actors.Remove(actor);
+        }
+
+        public void RemoveActors(List<Actor> actors)
+        {
+            m_actors.RemoveRange(0, m_actors.Count);
+        }
+    }
+}
